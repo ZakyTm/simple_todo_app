e131d344a569f1428b4ed77b512f0a95830450d4
ZakyTm <zakaria.t149919@gmail.com>
2024-07-23 01:29:15 +0100

    adding the dependency Slidable, creating the delete button and it's function, plus finishing up the app

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -9,6 +9,7 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  final _controller = TextEditingController();
   List toDoList = [
     ['Task 1', false],
   ];
@@ -19,40 +20,73 @@ class _HomePageState extends State<HomePage> {
     });
   }
 
+  void saveNewTask() {
+    setState(() {
+      toDoList.add([_controller.text, false]);
+      _controller.clear();
+    });
+  }
+
+  void deleteTask(int index) {
+    setState(() {
+      toDoList.removeAt(index);
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        backgroundColor: Colors.deepPurple[50],
-        appBar: AppBar(
-          title: const Text('Todo App'),
-          backgroundColor: Colors.blue[300],
-          foregroundColor: Colors.white,
-        ),
-        body: ListView.builder(
-            itemCount: toDoList.length,
-            itemBuilder: (BuildContext context, index) {
-              return TodoList(
-                  taskName: toDoList[index][0],
-                  isDone: toDoList[index][1],
-                  onChanged: (value) => checkBoxChanged(index));
-            }),
-        floatingActionButton: Row(
+      backgroundColor: Colors.deepPurple[50],
+      appBar: AppBar(
+        title: const Text('Todo App'),
+        backgroundColor: Colors.blue[300],
+        foregroundColor: Colors.white,
+      ),
+      body: ListView.builder(
+          itemCount: toDoList.length,
+          itemBuilder: (BuildContext context, index) {
+            return TodoList(
+              taskName: toDoList[index][0],
+              isDone: toDoList[index][1],
+              onChanged: (value) => checkBoxChanged(index),
+              deleteFunction: (value) => deleteTask(index),
+            );
+          }),
+      floatingActionButton: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 20),
+        child: Row(
           children: [
-            const Expanded(
-                child: Padding(
-              padding: EdgeInsets.symmetric(horizontal: 20),
-              child: TextField(),
-            )),
+            Expanded(
+              child: Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 20),
+                child: TextField(
+                    controller: _controller,
+                    decoration: InputDecoration(
+                      hintText: 'Add a new Todo Item',
+                      filled: true,
+                      fillColor: Colors.blue[300],
+                      enabledBorder: const OutlineInputBorder(
+                        borderSide: BorderSide(
+                          color: Colors.white,
+                        ),
+                      ),
+                      focusedBorder: OutlineInputBorder(
+                        borderSide: const BorderSide(
+                          color: Colors.white,
+                        ),
+                        borderRadius: BorderRadius.circular(15),
+                      ),
+                    )),
+              ),
+            ),
             FloatingActionButton(
-              onPressed: () {
-                setState(() {
-                  toDoList.add([' ZTask ', false]);
-                });
-              },
+              onPressed: saveNewTask,
               child: const Icon(Icons.add),
               backgroundColor: Colors.blue[300],
             ),
           ],
-        ));
+        ),
+      ),
+    );
   }
 }
--- lib/utils/todo_list.dart
+++ lib/utils/todo_list.dart
@@ -1,44 +1,66 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_slidable/flutter_slidable.dart';
 
 class TodoList extends StatelessWidget {
   const TodoList(
       {super.key,
       required this.taskName,
       required this.isDone,
-      this.onChanged});
+      required this.onChanged,
+      required this.deleteFunction});
 
   final String taskName;
   final bool isDone;
   final Function(bool?)? onChanged;
+  final Function(BuildContext)? deleteFunction;
 
   @override
   Widget build(BuildContext context) {
     return Padding(
-        padding: const EdgeInsets.all(20),
-        child: Container(
-          padding: const EdgeInsets.all(20),
-          decoration: BoxDecoration(
-            color: Colors.blue[300],
-            borderRadius: BorderRadius.circular(15),
-          ),
-          child: Row(
+        padding: const EdgeInsets.only(
+          left: 20,
+          right: 20,
+          top: 20,
+          bottom: 0,
+        ),
+        child: Slidable(
+          endActionPane: ActionPane(
+            motion: const StretchMotion(),
             children: [
-              Checkbox(
-                  value: isDone,
-                  onChanged: onChanged,
-                  activeColor: Colors.black,
-                  checkColor: Colors.white),
-              Text(
-                taskName,
-                style: TextStyle(
-                  fontSize: 20,
-                  fontWeight: FontWeight.bold,
-                  decoration: isDone ? TextDecoration.lineThrough : null,
-                  decorationColor: Colors.black,
-                  decorationThickness: 2.85,
-                ),
+              SlidableAction(
+                onPressed: deleteFunction,
+                icon: Icons.delete,
+                backgroundColor: Colors.red,
+                foregroundColor: Colors.white,
+                borderRadius: BorderRadius.circular(15),
               ),
             ],
+          ),
+          child: Container(
+            padding: const EdgeInsets.all(20),
+            decoration: BoxDecoration(
+              color: Colors.blue[300],
+              borderRadius: BorderRadius.circular(15),
+            ),
+            child: Row(
+              children: [
+                Checkbox(
+                    value: isDone,
+                    onChanged: onChanged,
+                    activeColor: Colors.black,
+                    checkColor: Colors.white),
+                Text(
+                  taskName,
+                  style: TextStyle(
+                    fontSize: 20,
+                    fontWeight: FontWeight.bold,
+                    decoration: isDone ? TextDecoration.lineThrough : null,
+                    decorationColor: Colors.black,
+                    decorationThickness: 2.85,
+                  ),
+                ),
+              ],
+            ),
           ),
         ));
   }
